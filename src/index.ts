fe5df8f6e9f9efd966752fb00a5c380fc1452de2
Christian Leicht <christianlj@ltaps.dk>
2019-03-05 10:53:20 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -28,9 +28,6 @@ export default class RPI {
 
         document.addEventListener('scroll', () => {
             this.scrolling = true;
-        }, {
-            capture: true,
-            passive: true
         });
 
         setInterval(() => {

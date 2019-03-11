e89c3efcc5e99a43e8c165db78d16c430a386cdd
Christian Leicht <christianlj@ltaps.dk>
2019-03-11 14:07:12 +0100

    .

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,38 @@
-!function(root,factory){"object"==typeof exports&&"object"==typeof module?module.exports=factory():"function"==typeof define&&define.amd?define("Kadmos",[],factory):"object"==typeof exports?exports.Kadmos=factory():root.Kadmos=factory()}(window,function(){return function(modules){var installedModules={};function __webpack_require__(moduleId){if(installedModules[moduleId])return installedModules[moduleId].exports;var module=installedModules[moduleId]={i:moduleId,l:!1,exports:{}};return modules[moduleId].call(module.exports,module,module.exports,__webpack_require__),module.l=!0,module.exports}return __webpack_require__.m=modules,__webpack_require__.c=installedModules,__webpack_require__.d=function(exports,name,getter){__webpack_require__.o(exports,name)||Object.defineProperty(exports,name,{enumerable:!0,get:getter})},__webpack_require__.r=function(exports){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(exports,"__esModule",{value:!0})},__webpack_require__.t=function(value,mode){if(1&mode&&(value=__webpack_require__(value)),8&mode)return value;if(4&mode&&"object"==typeof value&&value&&value.__esModule)return value;var ns=Object.create(null);if(__webpack_require__.r(ns),Object.defineProperty(ns,"default",{enumerable:!0,value:value}),2&mode&&"string"!=typeof value)for(var key in value)__webpack_require__.d(ns,key,function(key){return value[key]}.bind(null,key));return ns},__webpack_require__.n=function(module){var getter=module&&module.__esModule?function(){return module.default}:function(){return module};return __webpack_require__.d(getter,"a",getter),getter},__webpack_require__.o=function(object,property){return Object.prototype.hasOwnProperty.call(object,property)},__webpack_require__.p="",__webpack_require__(__webpack_require__.s=0)}([function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var RPI=function(){function RPI(){}return RPI.getMax=function(){return window.document.body.scrollHeight-window.innerHeight+""},RPI.getValue=function(){return window.scrollY+""},RPI.getProgressBar=function(){return document.getElementsByTagName("progress")[0]},RPI.init=function(){var _this=this,progressBar=this.getProgressBar();null!=progressBar&&progressBar.setAttribute("max",this.getMax()),document.addEventListener("resize",function(){null!=progressBar&&(progressBar.setAttribute("max",_this.getMax()),progressBar.setAttribute("value",_this.getValue()))}),document.addEventListener("scroll",function(){_this.scrolling=!0}),setInterval(function(){_this.scrolling&&null!=progressBar&&(_this.scrolling=!1,progressBar.setAttribute("value",_this.getValue()))},this.scrollingDelay)},RPI.scrolling=!1,RPI.scrollingDelay=250,RPI}();__webpack_exports__.default=RPI}])});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class RPI {
+    static getMax() {
+        return (window.document.body.scrollHeight - window.innerHeight) + "";
+    }
+    static getValue() {
+        return window.scrollY + "";
+    }
+    static getProgressBar() {
+        return document.getElementsByTagName("progress")[0];
+    }
+    static init() {
+        const progressBar = this.getProgressBar();
+        if (progressBar != null) {
+            progressBar.setAttribute("max", this.getMax());
+        }
+        document.addEventListener('resize', () => {
+            if (progressBar != null) {
+                progressBar.setAttribute("max", this.getMax());
+                progressBar.setAttribute("value", this.getValue());
+            }
+        });
+        document.addEventListener('scroll', () => {
+            this.scrolling = true;
+        });
+        setInterval(() => {
+            if (this.scrolling && progressBar != null) {
+                this.scrolling = false;
+                progressBar.setAttribute("value", this.getValue());
+            }
+        }, this.scrollingDelay);
+    }
+}
+RPI.scrolling = false;
+RPI.scrollingDelay = 250;
+exports.default = RPI;
 //# sourceMappingURL=index.js.map
